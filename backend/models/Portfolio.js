cd0e02befc682fdd69583fd196426d0d246dd94a
Udeshika95 <udeshikamadubashini95@gmail.com>
2025-09-28 10:44:04 +0530

    Added new Order,Portfolio,Stock, Transation schemas

---
+++ backend/models/Portfolio.js
@@ -0,0 +1,7 @@
+const portfolioSchema = new mongoose.Schema({
+  buyer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // FK → Users
+  virtual_balance: { type: Number, default: 0 },
+  created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Portfolio', portfolioSchema);
