0dc663310416317a25bef48d5392c9d646a609ed
Asher <asherengland2497@gmail.com>
2025-10-02 16:07:30 +1000

    Adding baseHandler Function

---
+++ backend/handler/baseHandler.js
@@ -0,0 +1,21 @@
+// Abstract Handler -> create handler file
+// Base case
+class Handler {
+   // Link one handler -> next handler
+   setNext(handler) {
+      // Storing handler in this.nextHandler
+      this.nextHandler = handler;
+      // enables chaining / building chain -> return handler
+      return handler;
+   };
+
+   handle(request) {
+
+      if (this.nextHandler) {
+         return this.nextHandler.handle(request);
+      } 
+      return null;
+   };
+};
+
+module.exports = Handler;
