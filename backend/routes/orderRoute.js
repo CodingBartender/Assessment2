0a00c6bac2f950c90dad4c4df9c46eafe033addd
Udeshika95 <udeshikamadubhashini95@gmail.com>
2025-10-02 21:50:48 +0530

    rest pattern api endpoints

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 
 // CRUD for orders (restricted to logged-in buyer)
-router.post('/create', orderController.createOrder);
-router.get('/getOrdersByUserId/:id', orderController.getOrders); // get order by user id
-router.get('/totalQuantity/:buyerId', orderController.getTotalOrderQuantityByBuyer); // get total order quantity for buyer
+router.post('/', orderController.createOrder);
+router.get('/', orderController.getOrders); // get order by user id
+router.get('/:buyerId', orderController.getTotalOrderQuantityByBuyer); // get total order quantity for buyer
 router.get('/getbyOrderIdAndUserID/:id', orderController.getOrderById);
 router.put('/:id', orderController.updateOrder);
 router.delete('/delete/:id', orderController.deleteOrder);
