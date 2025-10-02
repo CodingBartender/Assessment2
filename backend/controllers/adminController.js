39e55308dc23717ec165a7840f3990fe2e9d3463
Lekshmi Sindhu <lekshmiscb@gmail.com>
2025-10-03 09:07:18 +1000

    Updated adminController.js

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,53 @@
+// FR-13: Administrative Control 
+
+
+const User = require("../models/User");
+const Stock = require("../models/Stock");
+
+class AdminController {
+  static instance;
+
+  constructor() {
+
+    // Singleton
+    if (AdminController.instance) return AdminController.instance;
+    AdminController.instance = this;
+
+
+  }
+
+  // Factory methods 
+  async createUser({ name, email, password, role }) {
+
+    // Factory - builds a User object from params
+    return await User.create({ name, email, password, role });
+  }
+
+  async createStock(stockData) {
+
+    // Factory - builds a Stock object from params
+    return await Stock.create(stockData);
+  }
+
+  // user management 
+  async getAllUsers() {
+    return await User.find();
+  }
+
+  async deleteUser(userId) {
+    return await User.findByIdAndDelete(userId);
+  }
+
+  // stock management
+  async getAllStocks() {
+    return await Stock.find();
+  }
+
+  async deleteStock(stockId) {
+    return await Stock.findByIdAndDelete(stockId);
+  }
+
+
+}
+
+module.exports = new AdminController(); // Singleton instance
