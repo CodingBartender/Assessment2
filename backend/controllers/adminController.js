d8529dd95f3578ded245c5f15d6fea0b6da46e62
Lekshmi Sindhu <lekshmiscb@gmail.com>
2025-10-03 14:59:31 +1000

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -8,36 +8,26 @@ class AdminController {
   static instance;
 
   constructor() {
-
     // Singleton
     if (AdminController.instance) return AdminController.instance;
     AdminController.instance = this;
-
-
   }
-
   // Factory methods 
   async createUser({ name, email, password, role }) {
-
     // Factory - builds a User object from params
     return await User.create({ name, email, password, role });
   }
-
-
   // user management 
   async getAllUsers() {
     return await User.find();
   }
-
   async deleteUser(userId) {
     return await User.findByIdAndDelete(userId);
   }
-
   // stock management
   async getAllStocks() {
     return await Stock.find();
   }
-
   async deleteStock(stockId) {
     return await Stock.findByIdAndDelete(stockId);
   }
